a1629bb49414301f6a7e67f12a2645926218c556
Raquel Randall <raquel.sae.randall@gmail.com>
2018-10-08 21:19:22 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -40,7 +40,15 @@ def count_contestants_by_hometown(data, hometown)
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, contestants|
+    contestants.each do |attribute|
+      attribute.each do |key, value|
+        if value == hometown
+          return attribute["occupation"]
+        end
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
